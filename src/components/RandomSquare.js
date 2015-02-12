ee1c14b123ca831603cd217be4bdeb2e6bcae05a
sole <sole@mozilla.com>
2015-02-12 18:15:12 +0000

    add 'pure' web component

---
+++ src/components/RandomSquare.js
@@ -0,0 +1,32 @@
+var componentPrototype = Object.create(HTMLElement.prototype);
+
+componentPrototype.createdCallback = function() {
+	var canvas = document.createElement('canvas');
+	canvas.width = '100';
+	canvas.height = '100';
+	this._canvas = canvas;
+	this.appendChild(canvas);
+	this.randomise();
+};
+
+componentPrototype.randomise = function() {
+	var canvas = this._canvas;
+	var w = canvas.width;
+	var h = canvas.height;
+	var ctx = canvas.getContext('2d');
+	var randomColour = Math.floor(0xFFFFFF * Math.random());
+	var hexColour = '#' + randomColour.toString(16);
+	ctx.fillStyle = hexColour;
+	ctx.fillRect(0, 0, w, h);
+};
+
+function register(name) {
+	return document.registerElement(name, {
+		prototype: componentPrototype
+	});
+}
+
+module.exports = {
+	register: register
+};
+
--- src/jquery/js/main.js
+++ src/jquery/js/main.js
@@ -1,19 +1,37 @@
+var RandomSquare = require('../../components/RandomSquare');
 var bellButton = require('../../components/bellButton');
 var $ = require('jquery');
 
+RandomSquare.register('random-square');
 bellButton.register('bell-button');
 
+// Instancing totally custom element
+(function() {
+	// 1) with native document.createElement
+	var instanceA = document.createElement('random-square');
+	$('body').append(instanceA);
+
+	// 2) with jQuery's instantiation magic - TODO: does it use innerHTML internally?
+	var instanceB = $('<random-square>');
+	$('body').append(instanceB);
+})();
+
 // Instancing custom element that extends an existing element: bell-button < button
+(function() {
+	// 1) with native document.createElement
+	var instanceA = document.createElement('button', 'bell-button');
+	$('body').append(instanceA);
 
-// 1) with native document.createElement
-var instanceA = document.createElement('button', 'bell-button');
-$('body').append(instanceA);
+	// 2) with jQuery's instantiation magic
+	var instanceB = $('<button is="bell-button"></button>');
+	$('body').append(instanceB);
 
-// 2) with jQuery's instantiation magic - TODO: does it use innerHTML internally?
-var instanceB = $('<button is="bell-button"></button>');
-$('body').append(instanceB);
+	// 3) but this doesn't work. The proper prototype doesn't seem to be used, an "empty" button is rendered
+	var instanceC = $('<button>');
+	instanceC.attr('is', 'bell-button');
+	$('body').append(instanceC);
+})();
 
-// 3) but this doesn't work. The proper prototype doesn't seem to be used.
-var instanceC = $('<button>');
-instanceC.attr('is', 'bell-button');
-$('body').append(instanceC);
+// TODO: attributes
+// TODO: callbacks
+// TODO: calling methods on the instance
