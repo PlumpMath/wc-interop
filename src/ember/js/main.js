a3047d6c9d336bcf67d646d219c54cadc172c9bd
sole <sole@mozilla.com>
2015-02-18 11:42:31 +0000

    getting the ember mole rolling... slowly

--- src/ember/js/main.js
+++ src/ember/js/main.js
@@ -1,8 +1,19 @@
 var RandomSquare = require('../../components/RandomSquare');
 var BellButton = require('../../components/BellButton');
 
-
 RandomSquare.register('random-square');
 BellButton.register('bell-button');
 
-console.log(Ember);
+//
+
+window.App = Ember.Application.create();
+
+/*App.Router.map(function() {
+	this.resource('wcinterop', { path: '/' });
+});*/
+
+App.ApplicationController = Ember.Controller.extend({
+	randomSquare: '<random-square></random-square>',
+	randomSquareWithAttributes: '<random-square width="50" height="25"></random-square>'
+
+});
