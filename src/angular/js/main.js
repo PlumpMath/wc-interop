cb875a3f6a49b5241b7bc51ed1bf37e07220d84f
sole <sole@mozilla.com>
2015-02-20 11:17:06 +0000

    more AngularJS surprises

--- src/angular/js/main.js
+++ src/angular/js/main.js
@@ -3,18 +3,22 @@ require('angular');
 var angular = window.angular;
 
 var RandomSquare = require('../../components/RandomSquare');
-//var BellButton = require('../../components/BellButton');
+var BellButton = require('../../components/BellButton');
 
 RandomSquare.register('random-square');
-//BellButton.register('bell-button');
+BellButton.register('bell-button');
 
 angular.module('InteropApp', [])
 	.controller('InteropController', ['$scope', function($scope) {
 		
 		$scope.squares = [
 			{ width: 50, height: 50 },
-			//{ width: 200, height: 25 },
-			//{ width: 100, height: 100 },
+			{ width: 200, height: 25 },
+			{ width: 100, height: 100 },
+		];
+
+		$scope.bellbuttons = [
+			'<button is="bell-button"/>'
 		];
 
 	}]);
